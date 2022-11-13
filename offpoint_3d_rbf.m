baa2fb5e2b695f59103a372f8c6fb2dc1fefbeee
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-11-13 11:01:09 +0100

    fast eval

--- offpoint_3d_rbf.m
+++ offpoint_3d_rbf.m
@@ -1,8 +1,9 @@
+close all;
 N = 1000;
-delta = 1e-2;
+delta = 1e-3;
 sigma = 1;
 
-epsilon = 1;
+epsilon = 0.5;
 
 %rbf_func = @(r) exp(-((epsilon*r).^2));
 rbf_func = @(r) (1 - r ./ epsilon).^2 .* (r < epsilon) .* (4 * r / epsilon + 2);
@@ -28,6 +29,7 @@ sphere = ptcloud;
 %quiver3(sphere_x, sphere_y, sphere_z, normals(:,1),normals(:,2),normals(:,3))
 %normals = sphere; % Per definition
 
+max_min_dist = max(min(distanceMatrix(sphere, sphere) + 1000*diag(ones([size(sphere, 1) 1]))))
 
 delta_pos = sphere + delta*normals;
 delta_neg = sphere - delta*normals;
@@ -38,12 +40,18 @@ train_data_w = vertcat(zeros(size(sphere,1),1), sigma.*ones(size(delta_pos,1),1)
 figure;
 %scatter3(sphere(:,1), sphere(:,2), sphere(:,3))
 disp('Starting rbf interpolation')
-potential = rbfPU(train_data, train_data_w, rbf_func, 8, 0.1);
+
+
+potential = rbfPU(train_data, train_data_w, rbf_func, 8, 0.9/7); % (k, 0.9 / (k-1))
 disp('Finished interpolation construction')
 
-[x,y,z] = meshgrid([min(min(sphere)):0.01:max(max(sphere))]);
+[x,y,z] = meshgrid([min(min(sphere)):0.05:max(max(sphere))]);
+tic
 V = potential(horzcat(flatten(x), flatten(y), flatten(z)));
+toc
+
 V = reshape(V, size(x));
+
 
 
 s = isosurface(x,y,z,V, 0);
@@ -52,9 +60,11 @@ s = isosurface(x,y,z,V, 0);
 
 p = patch(s);
 isonormals(x,y,z,V,p)
-view(3);
 set(p,'FaceColor',[0.5 1 0.5]);  
 set(p,'EdgeColor','none');
-camlight;
-lighting gouraud;
+camlight('right', 'infinite');
+daspect([1 1 1]);
+view([90 0]);
+
+axis off;
 
