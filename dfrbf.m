0d5a39f8b66a03952c10180d5a44b67c2339deda
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-11-19 10:45:14 +0100

    change rbf and make finer mesh

--- dfrbf.m
+++ dfrbf.m
@@ -1,16 +1,33 @@
-epsilon = 5;
-rbf = @(r) exp(-((epsilon .* r) .^ 2));
-rbfgrad = @(x) -2.*epsilon.*epsilon.*x .* rbf(norm2(x));
+close all;
+epsilon = 1;
 
-N = 100;
+%rbf = @(r) exp(-((epsilon .* r) .^ 2));
+%rbfgrad = @(x) -2.*epsilon.*epsilon.*x .* rbf(norm2(x));
+
+syms x y z;
+dr = 1e-5;
+r = sqrt(x*x + y*y + z*z + dr);
+%func = (1 - r ./ epsilon).^2 .* (4 * r / epsilon + 2);
+func = (1 - r ./ epsilon).^2;
+%func = exp(-((epsilon .* r) .^ 2));
+rbfgrad_f = matlabFunction(gradient(func), 'file', 'rbfgrad_file.m');
+rbfgrad = @(x) rbfgrad_file(x(:,1)', x(:,2)', x(:,3)')' .* (norm2(x) < epsilon);
+
+rbfHessian_f = matlabFunction(hessian(func));
+rbfHessian = @(x) rbfHessianHandler(x, rbfHessian_f) .* reshape((norm2(x) < epsilon), 1, 1, []);
+
+
+N = 1000;
 
 points = 2.*SpherePoints(N);
 normals = points;
-%load homer.mat
-%points = ptcloud;
+load homer.mat
+points = ptcloud;
 tic
-A = constructA(points, points, epsilon);
+A = constructA(points, points, rbfHessian);
+toc
 f = flatten(normals');
+tic
 c = A \ f;
 toc
 
@@ -28,16 +45,25 @@ toc
 
 figure
 tic
-[x,y,z] = meshgrid(min(points,[],'all'):0.01:max(points,[],'all'));
+[x,y,z] = meshgrid(min(points,[],'all'):0.02:max(points,[],'all'));
 V = evalCFRBF(horzcat(flatten(x), flatten(y), flatten(z)), points, c, rbfgrad);
 V = reshape(V, size(x));
 toc
 hold on
-isosurface(x,y,z,V, 0);
-scatter3(points(:,1), points(:,2), points(:,3))
+s = isosurface(x,y,z,V, 0);
+p = patch(s);
+isonormals(x,y,z,V,p)
+set(p,'FaceColor',[0.5 1 0.5]);  
+set(p,'EdgeColor','none');
+camlight('right', 'infinite');
+daspect([1 1 1]);
+view([90 0]);
+axis off;
+%scatter3(points(:,1), points(:,2), points(:,3))
+%quiver3(points(:,1), points(:,2), points(:,3), normals(:, 1), normals(:, 2), normals(:, 3))
 hold off
 
-function H = rbfHessian(x, epsilon)
+function H = gaussRbfHessian(x, epsilon)
     H = zeros([3 3 size(x, 1)]);
     ex = exp(-((epsilon * norm2(x)).^2));
     H(1, 1, :) = epsilon^4 * x(:,1).^2 .* ex - 1/2 * epsilon^2 * ex;
@@ -58,16 +84,20 @@ function H = rbfHessian(x, epsilon)
     H = H.*4;
 end
 
-function A = constructA(x_train, x_eval, epsilon)
+function H = rbfHessianHandler(x, handle)
+    H = zeros([3 3 size(x, 1)]);
+    for i=1:size(x,1)
+        H(:,:,i) = handle(x(i,1), x(i,2), x(i,3));
+    end
+end
+
+function A = constructA(x_train, x_eval, rbfHessian)
     n_train = size(x_train, 1);
     n_eval = size(x_eval, 1);
     A = zeros([3*n_eval 3*n_train]);
     for i=1:n_eval
-        H = rbfHessian(x_eval(i,:) - x_train, epsilon);
+        H = rbfHessian(x_eval(i,:) - x_train);
         A((i-1)*3 + 1 : (i-1)*3 + 3, :) = reshape(H, [3 3*n_train]);
-        %for j=1:n
-        %    A((i-1)*3 + 1 : (i-1)*3 + 3, (j-1)*3 + 1 : (j-1)*3 + 3) = H(j, :, :);
-        %end
     end
 end
 
